15d039e08cd9b2f22e4c1a35fe3f8bcddd7a8124
tamerlan806 <feofelact@gmail.com>
2025-02-13 20:42:32 +0200

    Update fog.lua

--- main/fog.lua
+++ main/fog.lua
@@ -1,7 +1,33 @@
-pcall(function()
-    local lighting = game:GetService("Lighting")
-    lighting.FogEnd = 1e10
-    lighting.Changed:Connect(function()
-        lighting.FogEnd = 1e10
-    end)
-end)
+local Lighting = game:GetService("Lighting")
+local RunService = game:GetService("RunService")
+
+local fullbrightEnabled = false
+
+local function applyFullbright()
+    Lighting.Brightness = 2
+    Lighting.ClockTime = 12
+    Lighting.FogEnd = 1e10
+    Lighting.GlobalShadows = false
+    Lighting.Ambient = Color3.fromRGB(255, 255, 255)
+end
+
+local function resetLighting()
+    Lighting.Brightness = 1
+    Lighting.ClockTime = 14
+    Lighting.FogEnd = 100000
+    Lighting.GlobalShadows = true
+    Lighting.Ambient = Color3.fromRGB(100, 100, 100)
+end
+
+local function toggleFullbright()
+    fullbrightEnabled = not fullbrightEnabled
+    if fullbrightEnabled then
+        applyFullbright()
+        RunService:BindToRenderStep("Fullbright", Enum.RenderPriority.Camera.Value, applyFullbright)
+    else
+        RunService:UnbindFromRenderStep("Fullbright")
+        resetLighting()
+    end
+end
+
+toggleFullbright()
